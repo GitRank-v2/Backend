41773fc298a31e29bbf137bc57cb79d1076cd527
김승진 <ohksj77@gmail.com>
2024-08-05 19:12:39 +0900

    setting: change gradle flyway dependency

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -29,8 +29,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-mysql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
